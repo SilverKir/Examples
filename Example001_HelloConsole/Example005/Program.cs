abb9cea598bb2acd068f2b0b1e0470a85669aa64
silver_kir <rci@mp03.ru>
2023-10-09 08:47:26 +0800

    Exsample5 If

---
+++ Example001_HelloConsole/Example005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("Введите имя пользовател: ");
+string username = Console.ReadLine();
+if (username.ToLower == "маша")
+{
+    Console.WriteLine (" Приветствую Маша!");
+}
+else
+{
+    Console.WriteLine ("Привет " + username );
+}
